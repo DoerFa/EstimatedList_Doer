f0c7a45d19a1a7dccf580c20f2b46ef84a508164
zhaofa-hp <zhaofa@huapubj.com>
2020-10-08 21:22:37 +0800

    Revert "222"
    This reverts commit 34b59b5c82330342e86e316bb527945665028144.

--- EstimatedDemoForSwift/EstimatedDemoForSwift/AppDelegate.swift
+++ EstimatedDemoForSwift/EstimatedDemoForSwift/AppDelegate.swift
@@ -14,9 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        ///222 要开始做一些骚操作了
-        
-        
+        // Override point for customization after application launch.
         return true
     }
 
