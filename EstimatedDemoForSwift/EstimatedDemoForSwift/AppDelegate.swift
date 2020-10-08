34b59b5c82330342e86e316bb527945665028144
zhaofa-hp <zhaofa@huapubj.com>
2020-10-08 21:22:04 +0800

    222

--- EstimatedDemoForSwift/EstimatedDemoForSwift/AppDelegate.swift
+++ EstimatedDemoForSwift/EstimatedDemoForSwift/AppDelegate.swift
@@ -14,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        ///222 要开始做一些骚操作了
+        
+        
         return true
     }
 
